080c3d6d668fb305def03f765fea16d339f8dff5
tenmo2003 <tenmo2003@gmail.com>
2023-11-15 14:48:49 +0700

    redefine route to separate screen

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,25 @@ import NotFound from "./pages/NotFound";
 import Sidebar from "./navs/Sidebar";
 import Login from "./pages/Login";
 function App() {
-  return (
-    <BrowserRouter>
+  const getPage = (children: React.ReactNode) => {
+    return (
       <div className="flex">
-        <div className="flex-1 h-screen">
+        <div className="h-screen flex-1">
           <Sidebar />
         </div>
-        <div className="flex-[5]">
-          <Routes>
-            <Route index element={<Home />}></Route>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/posts" element={<Posts />}></Route>
-            <Route path="/posts/:id" element={<Post />}></Route>
-            <Route path="*" element={<NotFound />}></Route>
-          </Routes>
-        </div>
+        <div className="flex-[5]">{children}</div>
       </div>
+    );
+  };
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={getPage(<Home />)}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/posts" element={getPage(<Posts />)}></Route>
+        <Route path="/posts/:id" element={getPage(<Post />)}></Route>
+        <Route path="*" element={getPage(<NotFound />)}></Route>
+      </Routes>
     </BrowserRouter>
   );
 }
