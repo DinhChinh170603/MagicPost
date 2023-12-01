1c069d7d0e4305f55b01386b6333e99ecf97da6f
tenmo2003 <tenmo2003@gmail.com>
2023-12-01 10:27:55 +0700

    fixed login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -36,7 +36,7 @@ export default function Login() {
   };
 
   return (
-    <Form form={form} onFinish={onFinish} className="flex h-full w-full">
+    <Form form={form} onFinish={onFinish} className="flex h-screen w-full">
       <div className="flex flex-1 flex-col items-center justify-center bg-gray-300">
         <img src={logo} className="h-[50%] w-[50%]" alt="logo" />
       </div>
