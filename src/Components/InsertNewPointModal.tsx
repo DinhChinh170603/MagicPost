b6962244a3d53bf38927904fd4d3c0b7bb4233c9
DinhChinh170603 <21020288@vnu.edu.vn>
2023-12-13 01:59:20 +0700

    location.json, make detail Location for InsertNewPoint

--- src/Components/InsertNewPointModal.tsx
+++ src/Components/InsertNewPointModal.tsx
@@ -1,8 +1,8 @@
 import { Modal as AntModal, Form, Input, Select } from "antd";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { toast } from "react-toastify";
 import Loading from "../helpers/Loading";
-import service from "../helpers/service";
+import service, { getCities, getDistrictsByCityName, getWardsByCityAndDistrictName } from "../helpers/service";
 
 interface ModalProps {
   onSubmit: () => void;
@@ -19,10 +19,47 @@ const InsertNewPointModal: React.FC<ModalProps> = ({
 }) => {
   const [form] = Form.useForm();
 
+  const [cities, setCities] = useState([]);
+  const [districts, setDistricts] = useState([]);
+  const [wards, setWards] = useState([]);
+  const [selectedCity, setSelectedCity] = useState(null);
+  const [selectedDistrict, setSelectedDistrict] = useState(null);
+  const [selectedWard, setSelectedWard] = useState(null);
+
   const [loading, setLoading] = useState(false);
 
+  const fetchCitiesData = async () => {
+    // Read data from location.json
+    const locationData = await getCities();
+    setCities(locationData);
+  };
+
+  const handleCityChange = async (cityName) => {
+    await fetchCitiesData();
+    const districts = await getDistrictsByCityName(cityName);
+    setDistricts(districts);
+    setSelectedCity(cityName);
+    setSelectedDistrict(null);
+    setSelectedWard(null);
+  };
+  
+  const handleDistrictChange = async (districtName) => {
+    const newWards = await getWardsByCityAndDistrictName(selectedCity, districtName);
+    setWards(newWards);
+    setSelectedDistrict(districtName);
+    setSelectedWard(null);
+  };
+
+  // Read data from file location.json when component was render
+  useEffect(() => {
+    fetchCitiesData();
+    setSelectedDistrict(null);
+    setSelectedWard(null);
+  }, [selectedCity]);
+
   const onFinish = () => {
-    const { name, location } = form.getFieldsValue();
+    const { name } = form.getFieldsValue();
+    const location = selectedCity + ", " + selectedDistrict + ", " + selectedWard
 
     setLoading(true);
 
@@ -58,33 +95,44 @@ const InsertNewPointModal: React.FC<ModalProps> = ({
           open={isOpen}
           onCancel={() => setModalOpen(false)}
         >
-          <div className="mb-8 text-2xl font-bold">Fill out this form</div>
+          <div className="mb-8 text-2xl font-bold">Create an Exchange Point</div>
           <Form.Item
-            className="w-[60%] flex-1"
+            className="mb-8 w-[70%] flex-1"
             name="name"
             label="Name"
             rules={[{ required: true, message: "Please enter a Name" }]}
           >
             <Input />
           </Form.Item>
-          <Form.Item
-            className="mb-8 h-8 w-[60%] flex-1"
-            name="location"
-            label="Location"
-            rules={[{ required: true, message: "Please select a location" }]}
-          >
-            <Select
-              defaultValue="Location"
-              options={[
-                { value: "Hà Nội", label: "Hà Nội" },
-                { value: "Hải Phòng", label: "Hải Phòng" },
-                { value: "Vinh", label: "Vinh" },
-                { value: "Biên Hòa", label: "Biên Hòa" },
-                { value: "Đà Nẵng", label: "Đà Nẵng" },
-                { value: "Hồ Chí Minh", label: "Hồ Chí Minh" },
-              ]}
-            />
-          </Form.Item>
+          <Form.Item className="mb-8 w-[70%] flex-1" label="City" name="city" rules={[{ required: true, message: "Please select a city" }]}>
+          <Select onChange={handleCityChange}>
+            {cities.map((city) => (
+              <Select.Option key={city.name} value={city.name}>
+                {city.name}
+              </Select.Option>
+            ))}
+          </Select>
+        </Form.Item>
+
+        <Form.Item className="mb-8 w-[70%] flex-1" label="District" name="district" rules={[{ required: true, message: "Please select a district" }]}>
+          <Select onChange={handleDistrictChange} disabled={!selectedCity}>
+            {districts.map((district) => (
+              <Select.Option key={district.name} value={district.name}>
+                {district.name}
+              </Select.Option>
+            ))}
+          </Select>
+        </Form.Item>
+
+        <Form.Item className="mb-8 w-[70%] flex-1" label="Ward" name="wards" rules={[{ required: true, message: "Please select a ward" }]}>
+          <Select onChange={setSelectedWard} disabled={!selectedDistrict}>
+            {wards.map((ward) => (
+              <Select.Option key={ward.name} value={ward.name}>
+                {ward.name}
+              </Select.Option>
+            ))}
+          </Select>
+        </Form.Item>
         </AntModal>
       </Form>
     </>
--- src/helpers/service.ts
+++ src/helpers/service.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 import { toast } from "react-toastify";
 import { roleValueMap } from "./helpers";
 
-const baseURL = "http://127.0.0.1:8080/api";
-// const baseURL = "https://magicpost.onrender.com/api";
+// const baseURL = "http://127.0.0.1:8080/api";
+const baseURL = "https://magicpost.onrender.com/api";
 
 const service = axios.create({
   baseURL: baseURL,
@@ -43,5 +43,50 @@ service.interceptors.response.use(
     return response;
   }
 );
+
+// Get cities
+export const getCities = async () => {
+  try {
+    const response = await axios.get('/location.json');
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching cities:", error);
+    throw error;
+  }
+};
+
+// Get districtsList base on cityName
+export const getDistrictsByCityName = async (cityName: string) => {
+  try {
+    const cities = await getCities();
+    const city = cities.find((c) => c.name === cityName);
+
+    if (city) {
+      return city.districts;
+    } else {
+      throw new Error(`City with name ${cityName} not found`);
+    }
+  } catch (error) {
+    console.error("Error fetching districts:", error);
+    throw error;
+  }
+};
+
+// Get wardsList base on cityName and districtName
+export const getWardsByCityAndDistrictName = async (cityName: string, districtName: string) => {
+  try {
+    const districts = await getDistrictsByCityName(cityName);
+    const district = districts.find((d) => d.name === districtName);
+
+    if (district) {
+      return district.wards;
+    } else {
+      throw new Error(`District with name ${districtName} not found`);
+    }
+  } catch (error) {
+    console.error("Error fetching wards:", error);
+    throw error;
+  }
+};
 
 export default service;
