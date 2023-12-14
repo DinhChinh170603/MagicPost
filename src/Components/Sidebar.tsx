d67cc459240cf0c540f978f4c42d120248f031fd
DinhChinh170603 <21020288@vnu.edu.vn>
2023-12-14 16:44:56 +0700

    DeliveryStatus for Manager_Exchange

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -125,6 +125,15 @@ const Sidebar = (props: any) => {
               Employees
             </NavLink>
             <NavLink
+              to="/invite-employee"
+              end
+              className={({ isActive }) =>
+                isActive ? activeStyle : inactiveStyle
+              }
+            >
+              Invite Employee
+            </NavLink>
+            <NavLink
               to="/package-management"
               end
               className={({ isActive }) =>
@@ -134,13 +143,13 @@ const Sidebar = (props: any) => {
               Package Management
             </NavLink>
             <NavLink
-              to="/invite-employee"
+              to="/delivery-status"
               end
               className={({ isActive }) =>
                 isActive ? activeStyle : inactiveStyle
               }
             >
-              Invite Employee
+              Delivery Status
             </NavLink>
           </>
         );
